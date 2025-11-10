89bc98b51c44345badb11d720d34bec3b2081804
mayurikole118 <mayukole118@gmail.com>
2025-11-10 13:33:20 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -299,7 +299,8 @@ app.get("/travel-stories/filter", authenticateToken, async (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
+
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
